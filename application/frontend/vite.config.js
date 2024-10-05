426b94a19c2b8a800554daa8314758590e6994de
Kenny Miller <63763541+knnymllr@users.noreply.github.com>
2024-10-04 21:40:33 -0400

    Integrate Docker for local development; Uninstall Vitest and reinstall as a dev dependency; Update .gitignore after successfully running Docker container

--- application/frontend/vite.config.js
+++ application/frontend/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // override default port 5173
+    // @see https://stackoverflow.com/a/74923755
+    // @see https://vitejs.dev/config/
+    port: 3000,
+    host: true,
+    strictPort: true,
+    watch: {
+      usePolling: true,
+    },
+  },
 })
