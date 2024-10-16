9d00427f2079b5dd27d7d29bcfc93cc38996c165
Kenny Miller <63763541+knnymllr@users.noreply.github.com>
2024-10-16 17:46:22 -0400

    Create DefaultLayout; Implement basic BrowserRouter structure; Relocate game components demo to placeholder Game component;

--- application/frontend/src/App.jsx
+++ application/frontend/src/App.jsx
@@ -1,20 +1,26 @@
-import './App.css'
-import './styles/App.scss'
-import Stack from './components/Stack'
-import RecyclingBin from './components/RecyclingBin'
-import GarbageCollector from './components/GarbageCollector'
+import "./App.css";
+import "./styles/App.scss";
+import { Routes, Route } from "react-router-dom";
+import DefaultLayout from "./layouts/DefaultLayout";
+import Game from "./components/Game";
 
-function App() {
-
+export default function App() {
   return (
-    <>
-      <section className="game">
-        <Stack />
-        <GarbageCollector />
-        <RecyclingBin />
-      </section>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route index element={<Game />} />
+        {/*
+         * All Routes use the DefaultLayout
+         * <Route index element={<Home />} />
+         * <Route path="lobby" element={<Lobby />} />
+         * path="watch"
+         * path="leaderboards"
+         * path="profile"
+         * path="dashboard"
+         * path="game/practice"
+         * path="game/[id]"
+         */}
+      </Route>
+    </Routes>
+  );
 }
-
-export default App
