fde4d4e0fdec623d3baec5135be036e4daae8952
Kenny Miller <63763541+knnymllr@users.noreply.github.com>
2024-10-16 19:33:39 -0400

    Add item cost prop to ItemEntry

--- application/frontend/src/components/dashboard-components/Store.jsx
+++ application/frontend/src/components/dashboard-components/Store.jsx
@@ -4,11 +4,11 @@ export default function Store() {
     return (
         <main>
             <article className="store">
-                <ItemEntry itemName={"Item 1"} description={"This is a generic description of item 1"}/>
-                <ItemEntry itemName={"Item 2"} description={"This is a generic description of item 2"}/>
-                <ItemEntry itemName={"Item 3"} description={"This is a generic description of item 3"}/>
-                <ItemEntry itemName={"Item 4"} description={"This is a generic description of item 4"}/>
-                <ItemEntry itemName={"Item 5"} description={"This is a generic description of item 5"}/>
+                <ItemEntry itemName={"Item 1"} itemDescription={"This is a generic description of item 1"} itemCost={"$10"}/>
+                <ItemEntry itemName={"Item 2"} itemDescription={"This is a generic description of item 2"} itemCost={"$12"}/>
+                <ItemEntry itemName={"Item 3"} itemDescription={"This is a generic description of item 3"} itemCost={"$8"}/>
+                <ItemEntry itemName={"Item 4"} itemDescription={"This is a generic description of item 4"} itemCost={"$20"}/>
+                <ItemEntry itemName={"Item 5"} itemDescription={"This is a generic description of item 5"} itemCost={"$15"}/>
             </article>
         </main>
     )
--- application/frontend/src/components/entries/ItemEntry.jsx
+++ application/frontend/src/components/entries/ItemEntry.jsx
@@ -1,12 +1,15 @@
 import viteLogo from '/vite.svg'
-export default function ItemEntry({ itemName, description }) {
+export default function ItemEntry({ itemName, itemDescription, itemCost }) {
     return (
         <section className={`${itemName} base-entry item-entry`}>
             <div className="item-img-container">
                 <img src={viteLogo} className="item-img" alt="Vite logo" />
                 <h1 className="item-name">{itemName}</h1>
             </div>
-            <p className="item-description">{description}</p>
+            <div className="item-description-container">
+                <p className="item-description">{itemDescription}</p>
+                <p className="item-cost">{itemCost}</p>
+            </div>
         </section>
     )
 };
