1463f62379f45460988ac070701622a0e72cc5f5
Kenny Miller <63763541+knnymllr@users.noreply.github.com>
2024-10-18 20:31:27 -0400

    Update react grid npm package; Placeholder store and dashboard styles; Placeholder money state for showing active and inactive buttons;

--- application/frontend/src/components/Dashboard.jsx
+++ application/frontend/src/components/Dashboard.jsx
@@ -1,5 +1,14 @@
 import Store from "./dashboard-components/Store";
 
 export default function Dashboard() {
-    return <Store />
-};
+  return (
+    <main className="main-dashboard">
+      <div className="inbox-placeholder"></div>
+      <Store />
+      <div className="game-history-placeholder"></div>
+      <div className="best-history-placeholder"></div>
+      <div className="settings-placeholder"></div>
+      <div className="stats-placeholder"></div>
+    </main>
+  );
+}
--- application/frontend/src/components/entries/ItemEntry.jsx
+++ application/frontend/src/components/entries/ItemEntry.jsx
@@ -1,15 +1,28 @@
-import viteLogo from '/vite.svg'
-export default function ItemEntry({ itemName, itemDescription, itemCost }) {
-    return (
-        <section className={`${itemName} base-entry item-entry`}>
-            <div className="item-img-container">
-                <img src={viteLogo} className="item-img" alt="Vite logo" />
-                <h1 className="item-name">{itemName}</h1>
-            </div>
-            <div className="item-description-container">
-                <p className="item-description">{itemDescription}</p>
-                <p className="item-cost">{itemCost}</p>
-            </div>
-        </section>
-    )
-};
+import { useState, useEffect } from "react";
+import viteLogo from "/vite.svg";
+export default function ItemEntry({
+  itemName,
+  itemDescription,
+  itemCost,
+  userMoney,
+}) {
+  const [btnColor, setBtnColor] = useState("blue");
+  useEffect(() => {
+    userMoney >= itemCost ? setBtnColor("blue") : setBtnColor("red");
+  }, [itemCost, userMoney]);
+
+  return (
+    <section className={`${itemName} base-entry item-entry`}>
+      <div className="item-img-container">
+        <img src={viteLogo} className="item-img" alt="Vite logo" />
+        <h1 className="item-name">{itemName}</h1>
+      </div>
+      <div className="item-description-container">
+        <p className="item-description">{itemDescription}</p>
+        <button id="btnItemCost" className={`btn-item-cost ${btnColor}`}>
+          ${itemCost}
+        </button>
+      </div>
+    </section>
+  );
+}
