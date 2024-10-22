47966821015200ad6b59ce7b6384535ecd573e3f
MinJoon Joo <tuk34896@temple.edu>
2024-10-22 13:51:51 -0400

    testing dp7 button component

--- application/frontend/src/App.jsx
+++ application/frontend/src/App.jsx
@@ -1,20 +1,30 @@
-import './App.css'
-import './styles/App.scss'
-import Stack from './components/Stack'
-import RecyclingBin from './components/RecyclingBin'
-import GarbageCollector from './components/GarbageCollector'
+import './App.css';
+import Button from './components/Button';
+import GarbageCollector from './components/GarbageCollector';
+import RecyclingBin from './components/RecyclingBin';
+import Stack from './components/Stack';
+import './styles/App.scss';
 
 function App() {
-
-  return (
-    <>
-      <section className="game">
-        <Stack />
-        <GarbageCollector />
-        <RecyclingBin />
-      </section>
-    </>
-  )
+    return (
+        <>
+            <section className="game">
+                <Stack />
+                <GarbageCollector />
+                <RecyclingBin />
+                <Button
+                    text="Click Me"
+                    onClick={() => console.log('First button clicked!')}
+                    className="button-primary"
+                />
+                <Button
+                    text="Another Button"
+                    onClick={() => console.log('Second button clicked!')}
+                    className="button-secondary"
+                />
+            </section>
+        </>
+    );
 }
 
-export default App
+export default App;
