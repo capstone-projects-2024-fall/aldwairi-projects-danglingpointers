bb269487e2cd2b3238bcf7489b58394c6c2ed5ee
Kenny Miller <63763541+knnymllr@users.noreply.github.com>
2024-10-23 08:54:57 -0400

    modify scss deprecation fix; nest Game in standalone route

--- application/frontend/src/App.jsx
+++ application/frontend/src/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
 
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
-        <Route index element={<Game />} />
+        <Route path="/game" element={<Game />} />
         {/*
          * All Routes use the DefaultLayout
          * <Route index element={<Home />} />
