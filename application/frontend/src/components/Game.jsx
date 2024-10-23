375ec6ba693e7cafc1f47eff4cefbcc850ccd610
Kenny Miller <63763541+knnymllr@users.noreply.github.com>
2024-10-23 10:21:05 -0400

    Add simple game over logic to refactor

--- application/frontend/src/components/Game.jsx
+++ application/frontend/src/components/Game.jsx
@@ -1,24 +1,31 @@
 import GarbageCollector from "./game-components/GarbageCollector";
 import RecyclingBin from "./game-components/RecyclingBin";
 import Stack from "./game-components/Stack";
+import convertSecondsToMinutes from "../scripts/convert-seconds-to-minutes";
 import { useEffect, useState } from "react";
 
 export default function Game() {
   const [timer, setTimer] = useState(0);
+  const [playingGame, setPlayingGame] = useState(true);
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      setTimer((prevSeconds) => prevSeconds + 1);
-    }, 1000);
+    let intervalId;
+    
+    if (playingGame) {
+      intervalId = setInterval(() => {
+        setTimer((prevSeconds) => prevSeconds + 1);
+      }, 1000);
+    }
 
     return () => clearInterval(intervalId);
-  }, []);
+  }, [playingGame]);
 
-  function convertSecondsToMinutes(timer) {
-    const minutes = Math.floor(timer / 60);
-    const seconds = timer % 60;
-    return `${minutes}:${seconds}`;
-  }
+  useEffect(() => {
+    if (timer > 10) {
+      setPlayingGame(false);
+      setTimer(-1)
+    }
+  }, [timer])
 
   return (
     <main>
