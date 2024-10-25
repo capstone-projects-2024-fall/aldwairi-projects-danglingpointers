00c10f00d3a32c581165113b30ec0941cc015537
Joe Stella <157044883+JoeStellaTU@users.noreply.github.com>
2024-10-24 21:38:32 -0400

    made description a filler feature

--- application/frontend/src/components/entries/WatchPreview.jsx
+++ application/frontend/src/components/entries/WatchPreview.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import GameEntry from './GameEntry';
 import '.../styles/watch-entry.scss';
 
-export default function WatchPreview({users, status, gameLink, "..."}){
+export default function WatchPreview({users, status, gameLink,videoDescription}){
     return(
         <div className = "watch-preview">
             <GameEntry users = {users} status={status} gameLink={gameLink}/>
-            <div className = "watch-feature">
-                //feature exclusive to watch listings
+            <div className = "description-container">
+                <p className="video-description">{videoDescription}</p>
             </div>
         </div>
 
