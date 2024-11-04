f2a5cf06eea7fd66e3fa252c5f4ea77193a44f49
MinJoon Joo <tuk34896@temple.edu>
2024-11-04 16:22:55 -0500

    Revert "Merge branch 'dp19-create-leaderboard-entry' into development"
    This reverts commit 04fda69b62ea26a927e478282f8c0dad9362049a, reversing
    changes made to 2a4dac4d4540c2ee8b6d4ae83e87327afbc701ad.

--- application/frontend/src/App.jsx
+++ application/frontend/src/App.jsx
@@ -1,41 +1,47 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Dashboard from './components/pages/Dashboard';
-import TestLeaderboardEntry from './components/pages/entries/TestLeaderboardEntry';
-import Game from './components/pages/Game';
-import Home from './components/pages/Home';
-import Leaderboards from './components/pages/Leaderboards';
-import Lobby from './components/pages/Lobby';
-import Profile from './components/pages/Profile';
-import Watch from './components/Watch';
-import { GameProvider } from './context/GameContext';
-import DefaultLayout from './layouts/DefaultLayout';
-import useUserAuthStore from './stores/userAuthStore';
-import useUserProfileStore from './stores/userProfileStore';
-import './styles/App.scss';
+import "./App.css";
+import "./styles/App.scss";
+import { Routes, Route, Navigate } from "react-router-dom";
+import DefaultLayout from "./layouts/DefaultLayout";
+import Game from "./components/pages/Game";
+import Dashboard from "./components/pages/Dashboard";
+import Profile from "./components/pages/Profile";
+import Home from "./components/pages/Home";
+import Watch from "./components/pages/Watch";
+import Lobby from "./components/pages/Lobby";
+import Leaderboards from "./components/pages/Leaderboards";
+import { GameProvider } from "./context/GameContext";
+import useUserAuthStore from "./stores/userAuthStore";
+import useUserProfileStore from "./stores/userProfileStore";
 
 export default function App() {
-    const { isLoggedIn, userId } = useUserAuthStore();
-    const { profileId } = useUserProfileStore();
-    return (
-        <Routes>
-            <Route path="/" element={<DefaultLayout />}>
-                <Route index element={<Home />} />
-                <Route
-                    path="game"
-                    element={
-                        <GameProvider>
-                            <Game />
-                        </GameProvider>
-                    }
-                />
-                <Route path="profile" element={<Profile userId={profileId ? profileId : userId ? userId : 1} />} />
-                <Route path="dashboard" element={isLoggedIn ? <Dashboard /> : <Navigate to="/" replace />} />
-                <Route path="watch" element={<Watch />} />
-                <Route path="lobby" element={<Lobby />} />
-                <Route path="leaderboards" element={<Leaderboards />} />
-                <Route path="test-leaderboard-entry" element={<TestLeaderboardEntry />} />
-            </Route>
-        </Routes>
-    );
+  const { isLoggedIn, userId } = useUserAuthStore();
+  const { profileId } = useUserProfileStore();
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route index element={<Home />} />
+        <Route
+          path="practice"
+          element={
+            <GameProvider>
+              <Game />
+            </GameProvider>
+          }
+        />
+        <Route
+          path="profile"
+          element={
+            <Profile userId={profileId ? profileId : userId ? userId : 1} />
+          }
+        />
+        <Route
+          path="dashboard"
+          element={isLoggedIn ? <Dashboard /> : <Navigate to="/" replace />}
+        />
+        <Route path="watch" element={<Watch />} />
+        <Route path="lobby" element={<Lobby />} />
+        <Route path="leaderboards" element={<Leaderboards />} />
+      </Route>
+    </Routes>
+  );
 }
