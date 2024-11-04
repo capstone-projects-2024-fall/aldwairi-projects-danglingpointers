185408885ae65f56fc1c611a4550221fe67e12ce
Kenny Miller <63763541+knnymllr@users.noreply.github.com>
2024-11-04 17:27:20 -0500

    Final path fixes; Clear import React and import stylesheets

--- application/frontend/src/components/entries/LeaderboardEntry.jsx
+++ application/frontend/src/components/entries/LeaderboardEntry.jsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
-import '../../styles/components/LeaderboardEntry.scss';
 
 export default function LeaderboardEntry({ user, score, timePlayed, date, gameLink }) {
     const formatDate = (dateString) => {
