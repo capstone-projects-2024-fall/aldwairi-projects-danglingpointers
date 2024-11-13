7a6bb7cdcb0d96197cbdcb7ecb09df18f4997600
Kenny Miller <63763541+knnymllr@users.noreply.github.com>
2024-11-13 11:49:26 -0500

    Add GameConsumer; Adjust routing;

--- application/backend/api/consumers.py
+++ application/backend/api/consumers.py
@@ -10,3 +10,30 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data):
         pass
+    
+class GameConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        pass
+
+    async def disconnect(self, close_code):
+        pass
+
+    async def receive(self, text_data):
+        pass
+    
+    async def game_message(self, event):
+        game_id = event['message_id']
+        self.send(text_data=json.dumps({
+            'type': 'game',
+            'game_id': game_id,
+        }))
+
+class ItemConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        pass
+
+    async def disconnect(self, close_code):
+        pass
+
+    async def receive(self, text_data):
+        pass
--- application/backend/api/routing.py
+++ application/backend/api/routing.py
@@ -7,5 +7,7 @@ let url = `ws://${window.location.host}/ws/socket-server/`;
 const chatSocket = new WebSocket(url);
 '''
 websocket_urlpatterns = [
-    re_path(r'ws/socket-server/', consumers.ChatConsumer.as_asgi()) 
+    re_path(r'ws/chat-server/', consumers.ChatConsumer.as_asgi()), 
+    re_path(r'ws/item-server/', consumers.ItemConsumer.as_asgi()), 
+    re_path(r'ws/game-server/', consumers.GameConsumer.as_asgi()) 
 ]
