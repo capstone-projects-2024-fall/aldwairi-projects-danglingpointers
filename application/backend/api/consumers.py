c018e049b3568ed70d75e8272e17c579b5ef6a94
MPags02 <30301995+MPags02@users.noreply.github.com>
2024-11-16 13:22:02 -0500

    Update receive method in GameConsumer to parse for game_message

--- application/backend/api/consumers.py
+++ application/backend/api/consumers.py
@@ -19,11 +19,20 @@ class GameConsumer(AsyncWebsocketConsumer):
         pass
 
     async def receive(self, text_data):
-        pass
-    
+        data = json.loads(text_data)
+
+        if data.get('type') == 'game_message':
+            game_id = data.get('game_id', None)  
+            if game_id:
+                await self.game_message({'message_id': game_id})
+            else:
+                await self.send(text_data=json.dumps({
+                    'error': 'game_id is required for game_message type.'
+                }))
+
     async def game_message(self, event):
         game_id = event['message_id']
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'type': 'game',
             'game_id': game_id,
         }))
