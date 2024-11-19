5e4ea73187ecd4982a45f0a10d44ff4fc8a99335
joshuajohnj <49101663+joshuajohnj@users.noreply.github.com>
2024-11-19 13:13:05 -0500

    finished recueve method

--- application/backend/api/consumers.py
+++ application/backend/api/consumers.py
@@ -1,6 +1,5 @@
 import json
-import asyncio
-from asgiref.sync import async_to_sync, sync_to_async
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -11,45 +10,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
         pass
 
     async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        if 'message_id' in text_data_json:
-            message_id = text_data_json['message_id']
-        message = text_data_json['message']
-
-        if text_data_json['type'] == 'approve':
-            async_to_sync(self.channel_layer.group_send)(
-                self.room_group_name,
-                {
-                    'type': 'approve_message',
-                    'message_id': message_id,
-                    'message': message
-                }
-            )
-        elif text_data_json['type'] == 'reject':
-            async_to_sync(self.channel_layer.group_send)(
-                self.room_group_name,
-                {
-                    'type': 'reject_message',
-                    'message': message
-                }
-            )
-        elif text_data_json['type'] == 'complete':
-            async_to_sync(self.channel_layer.group_send)(
-                self.room_group_name,
-                {
-                    'type': 'complete_message',
-                    'message_id': message_id,
-                    'message': message
-                }
-            )
-        else:
-            async_to_sync(self.channel_layer.group_send)(
-                self.room_group_name,
-                {
-                    'type': 'chat_message',
-                    'message': message
-                }
-            )
         pass
 class ItemConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -58,43 +18,16 @@ class ItemConsumer(AsyncWebsocketConsumer):
     async def disconnect(self, close_code):
         pass
 
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        if 'message_id' in text_data_json:
-            message_id = text_data_json['message_id']
-        message = text_data_json['message']
+    async def receive(self, item_data):
+        text_data_json = json.loads(item_data)
+        if 'item_id' in text_data_json:
+            item_id = text_data_json['item_id']
 
-        if text_data_json['type'] == 'approve':
+        if text_data_json['type'] == 'item':
             async_to_sync(self.channel_layer.group_send)(
                 self.room_group_name,
                 {
-                    'type': 'approve_message',
-                    'message_id': message_id,
-                    'message': message
-                }
-            )
-        elif text_data_json['type'] == 'reject':
-            async_to_sync(self.channel_layer.group_send)(
-                self.room_group_name,
-                {
-                    'type': 'reject_message',
-                    'message': message
-                }
-            )
-        elif text_data_json['type'] == 'complete':
-            async_to_sync(self.channel_layer.group_send)(
-                self.room_group_name,
-                {
-                    'type': 'complete_message',
-                    'message_id': message_id,
-                    'message': message
-                }
-            )
-        else:
-            async_to_sync(self.channel_layer.group_send)(
-                self.room_group_name,
-                {
-                    'type': 'item_message',
-                    'message': message
+                    'type': 'item',
+                    'message': item_id,
                 }
             )
