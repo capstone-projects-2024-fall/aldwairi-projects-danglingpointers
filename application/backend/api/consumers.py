09bde28eebdfadedda7fa01831f6f819d52c1bb9
Aaron Lee <20ajlee@gmail.com>
2024-11-19 11:10:14 -0500

    filled in receive func in chat consumer

--- application/backend/api/consumers.py
+++ application/backend/api/consumers.py
@@ -1,5 +1,6 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+from asgiref.sync import async_to_sync
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
