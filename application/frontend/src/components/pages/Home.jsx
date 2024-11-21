88d8e1e555aeb55c8e72d2066bb6eca430d48107
Kenny Miller <63763541+knnymllr@users.noreply.github.com>
2024-11-21 12:49:12 -0500

    Add react sockets npm; Add skelton for socket subscription

--- application/frontend/src/components/pages/Home.jsx
+++ application/frontend/src/components/pages/Home.jsx
@@ -1,36 +1,10 @@
 import LeaderboardsPreview from "../views/LeaderboardsPreview";
 import LobbyPreview from "../views/LobbyPreview";
 import WatchPreview from "../views/WatchPreview";
-import { GAME_URL } from "../../scripts/constants";
-import { useEffect, useState } from "react";
+// import { GAME_URL } from "../../scripts/constants";
+// import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [gameSocket, setGameSocket] = useState(null);
-  // const [gameId, setGameId] = useState(null);
-  
-  useEffect(() => {
-    setGameSocket(new WebSocket(GAME_URL));
-
-    gameSocket.onopen = () => {
-      console.log("Connected to game server");
-    };
-
-    gameSocket.onmessage = (event) => {
-      const data = JSON.parse(event.data);
-
-      if (data.type === "game") {
-        console.log(`${data.game_id} completed!`);
-      }
-    };
-
-    return () => {
-      if (gameSocket) {
-        gameSocket.close();
-      }
-    };
-
-  }, [gameSocket])
-
   return (
     <main className="main-home">
       <div className="preview-container">
--- application/frontend/src/scripts/constants.js
+++ application/frontend/src/scripts/constants.js
@@ -1,4 +1,4 @@
-export const HOST_PATH = `http://${window.location.host}/api`;
-export const GAME_URL = `ws://${window.location.host}/ws/game-server/`;
-export const ITEM_URL = `ws://${window.location.host}/ws/item-server/`;
-export const MESSAGE_URL = `ws://${window.location.host}/ws/message-server/`;
+export const HOST_PATH = `http://localhost:8000/api`;
+export const GAME_URL = `ws://localhost:8000/ws/game-server/`;
+export const ITEM_URL = `ws://localhost:8000/ws/item-server/`;
+export const MESSAGE_URL = `ws://localhost:8000/ws/message-server/`;
